79c2ad21c0662a2d3ffc0c4458f706fd2ffd8d6e
getToTheChopin <alan.ang@mail.mcgill.ca>
2018-08-04 10:07:51 -0400

    solve 2 more exercises

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,18 @@
-var leapYears = function() {
+var leapYears = function(inputYear) {
 
+    let outputBoolean = false;
+
+    if(inputYear % 4 === 0){
+
+        outputBoolean = true;
+
+        if(inputYear % 100 === 0 && inputYear % 400 !== 0) {
+            outputBoolean = false;
+        }
+
+    }
+    
+    return outputBoolean;
 }
 
 module.exports = leapYears
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-var ftoc = function() {
+var ftoc = function(inputF) {
+
+  let outputC = 0;
+
+  outputC = (inputF - 32) / 1.8;
+  outputC = Math.round(outputC * 10) / 10;
+
+  return outputC;
   
 }
 
-var ctof = function() {
+var ctof = function(inputC) {
   
+  let outputF = 0;
+
+  outputF = inputC * 1.8 + 32;
+  outputF = Math.round(outputF * 10) / 10;
+
+  return outputF;
+
 }
 
 module.exports = {
